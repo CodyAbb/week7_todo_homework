0a0f403b8d9235ca523ca25d0d0c7ced97d326cf
Cody Abbott <cody.abbott16@gmail.com>
2020-01-13 15:51:44 +0000

    able to list data in ul

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("Working nae bother");
+  new Vue({
+    el: "#app",
+    data: {
+      todos: ["Buy shopping", "Clean bathroom", "Car's MOT"]
+    },
+    methods: {
+
+    }
+  });
+})
