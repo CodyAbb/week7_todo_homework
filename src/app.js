11ed70a4ff90c635151c31d329c62cbdbc214752
Cody Abbott <cody.abbott16@gmail.com>
2020-01-13 16:07:33 +0000

    able to add a new todo

--- src/app.js
+++ src/app.js
@@ -5,10 +5,14 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-      todos: ["Buy shopping", "Clean bathroom", "Car's MOT"]
+      todos: ["Buy shopping", "Clean bathroom", "Car's MOT"],
+      newTodo: ""
     },
     methods: {
-
+      saveNewTodo: function () {
+        this.todos.push(this.newTodo);
+        this.newTodo = "";
+      }
     }
   });
 })
